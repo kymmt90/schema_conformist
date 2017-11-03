316bd98ed6d922b52c184ee5e22205151ac2c7c2
Kohei Yamamoto <kymmt90@gmail.com>
2017-11-04 00:31:35 +0900

    Add test cases

--- test/dummy/test/controllers/products_controller_test.rb
+++ test/dummy/test/controllers/products_controller_test.rb
@@ -1,38 +1,29 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @product = products(:one)
-  end
-
-  test "should get index" do
+  test 'GET /products' do
     get products_url, as: :json
-    assert_response :success
   end
 
-  test "should create product" do
+  test 'GET /products/:id' do
+    get product_url(products(:one)), as: :json
+  end
+
+  test "POST /products" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { name: @product.name, price: @product.price, stock_count: @product.stock_count } }, as: :json
+      post products_url, params: { product: { name: 'new product', price: 100, stock_count: 100 } }, as: :json
     end
-
-    assert_response 201
   end
 
-  test "should show product" do
-    get product_url(@product), as: :json
-    assert_response :success
-  end
-
-  test "should update product" do
-    patch product_url(@product), params: { product: { name: @product.name, price: @product.price, stock_count: @product.stock_count } }, as: :json
+  test "PUT /products/:id" do
+    product = products(:one)
+    put product_url(product), params: { product: { name: product.name, price: product.price, stock_count: 100 } }, as: :json
     assert_response 200
   end
 
-  test "should destroy product" do
+  test "DELETE /products/:id" do
     assert_difference('Product.count', -1) do
-      delete product_url(@product), as: :json
+      delete product_url(products(:one)), as: :json
     end
-
-    assert_response 204
   end
 end
