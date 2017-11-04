353280a4954dada9c6711453728cb505f987ecc8
Kohei Yamamoto <kymmt90@gmail.com>
2017-11-04 18:17:48 +0900

    Add a test case for unknown committee drivers

--- test/schema_conformist_test.rb
+++ test/schema_conformist_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class SchemaConformist::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, SchemaConformist
+  class DriverTestClass < Object
+    include SchemaConformist::Driver
+  end
+
+  setup do
+    Rails.application.config.schema_conformist.driver = :unknown_driver
+  end
+
+  teardown do
+    Rails.application.config.schema_conformist.driver = :open_api_2
+  end
+
+  test 'unknown driver is specified' do
+    assert_raises(SchemaConformist::Error) { DriverTestClass.new.driver }
   end
 end
