d12a5324c567c24f96d99a54cb2340c781c6cb06
Kohei Yamamoto <kymmt90@gmail.com>
2017-11-05 15:07:04 +0900

    Gather up initializers

--- lib/schema_conformist/railtie.rb
+++ lib/schema_conformist/railtie.rb
@@ -4,16 +4,10 @@ module SchemaConformist
       config.schema_conformist = Configuration.new
     end
 
-    initializer 'config.schema_conformist.schema_path' do
-      config.schema_conformist.schema_path = nil
-    end
-
-    initializer 'config.schema_conformist.ignored_api_paths' do
-      config.schema_conformist.ignored_api_paths = []
-    end
-
-    initializer 'config.schema_conformist.driver' do
+    initializer 'config.schema_conformist' do
       config.schema_conformist.driver = :hyper_schema
+      config.schema_conformist.ignored_api_paths = []
+      config.schema_conformist.schema_path = nil
     end
   end
 end
