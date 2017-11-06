83cf8dafb785d2ede0766d8c52e92aa247175e1f
Kohei Yamamoto <kymmt90@gmail.com>
2017-11-06 20:00:56 +0900

    Initialize configurations in before_configuration block

--- lib/schema_conformist/railtie.rb
+++ lib/schema_conformist/railtie.rb
@@ -2,9 +2,6 @@ module SchemaConformist
   class Railtie < Rails::Railtie
     config.before_configuration do
       config.schema_conformist = Configuration.new
-    end
-
-    initializer 'config.schema_conformist' do
       config.schema_conformist.driver = :hyper_schema
       config.schema_conformist.ignored_api_paths = []
       config.schema_conformist.schema_path = nil
